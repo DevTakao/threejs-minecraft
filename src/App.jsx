b15d4fdc0ab08ca2c5ac427c5c4227ab6a58ae00
Taka <taka@visibleone.com>
2023-11-01 21:36:54 +0630

    feat: first person view camera

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Ground from "./components/Ground";
 import Player from "./components/Player";
+import FPV from "./components/FPV";
 
 function App() {
   return (
     <>
-      {/* <div>Outside Canvas</div> */}
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
+        <FPV />
         <Physics>
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-white z-50">+</div>
     </>
   );
 }
